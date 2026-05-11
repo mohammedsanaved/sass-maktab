5e5d7fa2540b587bf3d4958e1238f42fae8defbf
mohammedsanaved <mosanaved@gmail.com>
2026-05-11 22:00:48 +0530

    changes added in prisma.js

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,34 @@
-import {
-  PrismaClient,
-  PrismaClientOptions,
-} from '../../generated/prisma/client';
+// import {
+//   PrismaClient,
+//   PrismaClientOptions,
+// } from '../../generated/prisma/client';
 
-const prismaOptions: PrismaClientOptions = {
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
+// const prismaOptions: PrismaClientOptions = {
+//   datasources: {
+//     db: {
+//       url: process.env.DATABASE_URL,
+//     },
+//   },
+// };
+
+// const prisma = new PrismaClient(prismaOptions);
+// export default prisma;
+import { PrismaClient } from '@prisma/client';
+
+// Use a global variable to prevent multiple instances of Prisma Client 
+// in development (prevents "Too many connections" errors in MongoDB)
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
     },
-  },
-};
+  });
 
-const prisma = new PrismaClient(prismaOptions);
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
 export default prisma;
